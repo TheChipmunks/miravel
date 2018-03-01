004ac573b29e8c9d9870320eb3712204ba8e81d7
Lewolf <sergey.neskhodovskiy@gmail.com>
2018-03-01 13:33:18 +0200

    Fix default theme config

--- resources/themes/default/config.php
+++ resources/themes/default/config.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'vendor'   => (array) include(__DIR__ . 'config/vendor.php'),
+    'vendor'   => (array) include(__DIR__ . '/config/vendor.php'),
 
-    'manifest' => (array) include(__DIR__ . 'config/manifest.php'),
+    'manifest' => (array) include(__DIR__ . '/config/manifest.php'),
 ];
