dc9888e1a2c0900e13344f48daa6a2237947f041
Lewolf <sergey.neskhodovskiy@gmail.com>
2018-04-06 17:51:27 +0300

    fix mix

--- resources/themes/default/webpack.config.js
+++ resources/themes/default/webpack.config.js
@@ -3,7 +3,7 @@ let ComponentFactory = require('laravel-mix/src/components/ComponentFactory');
 new ComponentFactory().installAll();
 let WebpackConfig    = require('laravel-mix/src/builder/WebpackConfig');
 
-mix.Paths.setRootPath(path.resolve(__dirname));
+mix.setResourceRoot(path.resolve(__dirname));
 
 require(Mix.paths.mix());
 
