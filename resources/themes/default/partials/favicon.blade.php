f92484470699839a86e9886a923de885f7945017
Lewolf <sergey.neskhodovskiy@gmail.com>
2018-04-07 13:30:58 +0300

    remove references to mix() from templates

--- resources/themes/default/partials/favicon.blade.php
+++ resources/themes/default/partials/favicon.blade.php
@@ -1,11 +1,11 @@
-<link rel="apple-touch-icon" sizes="180x180" href="{{ mix('images/favicon/apple-touch-icon.png') }}">
-<link rel="icon" type="image/png" sizes="32x32" href="{{ mix('images/favicon/favicon-32x32.png') }}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ mix('images/favicon/favicon-16x16.png') }}">
-<link rel="manifest" href="{{ mix('site.webmanifest') }}">
-<link rel="mask-icon" href="{{ mix('images/favicon/safari-pinned-tab.svg') }}" color="#ffffff">
-<link rel="shortcut icon" href="{{ mix('images/favicon/favicon.ico') }}">
+<link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
+<link rel="manifest" href="site.webmanifest">
+<link rel="mask-icon" href="images/favicon/safari-pinned-tab.svg" color="#ffffff">
+<link rel="shortcut icon" href="images/favicon/favicon.ico">
 <meta name="apple-mobile-web-app-title" content="{{ config('app.name') }}">
 <meta name="application-name" content="{{ config('app.name') }}">
 <meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-config" content="{{ mix('browserconfig.xml') }}">
+<meta name="msapplication-config" content="browserconfig.xml">
 <meta name="theme-color" content="#ffffff">
