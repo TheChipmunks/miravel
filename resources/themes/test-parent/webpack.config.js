af5302267c21cf6e00b74be28c40ca53d31547ce
Lewolf <sergey.neskhodovskiy@gmail.com>
2018-04-22 18:20:28 +0300

    move root path definition to webpack.config.js

--- resources/themes/test-parent/webpack.config.js
+++ resources/themes/test-parent/webpack.config.js
@@ -2,6 +2,13 @@ let mix = require('../../../node_modules/laravel-mix/src/index');
 let ComponentFactory = require('../../../node_modules/laravel-mix/src/components/ComponentFactory');
 new ComponentFactory().installAll();
 
+let themepath = path.resolve(__dirname, './');
+Mix.paths.setRootPath(themepath)
+mix.setPublicPath(themepath);
+mix.options({
+    clearConsole: false
+});
+
 require(Mix.paths.mix());
 
 Mix.dispatch('init', Mix);
--- resources/themes/test-parent/webpack.mix.js
+++ resources/themes/test-parent/webpack.mix.js
@@ -1,11 +1,4 @@
 let mix = require('laravel-mix');
 
-let themepath = path.resolve(__dirname, './');
-Mix.paths.setRootPath(themepath)
-mix.setPublicPath(themepath);
-mix.options({
-    clearConsole: false
-});
-
 mix .sass('assets/src/style.scss', 'assets/dist')
     .scripts('assets/src/script.js', 'assets/dist/script.js');
