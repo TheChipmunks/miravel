5b0f21ea6c4d61f444a1da164476904eab91ccde
Lewolf <sergey.neskhodovskiy@gmail.com>
2018-05-04 15:41:57 +0300

    reverting webpack.config.js

--- mix/webpack.config.js
+++ mix/webpack.config.js
@@ -15,14 +15,13 @@ mix.options({
     clearConsole: false
 });
 
-console.log('LaravelMixPath: ' + LaravelMixPath);
-console.log('global.LaravelMixPath: ' + global.LaravelMixPath);
-
-fs.exists(global.LaravelMixPath + '/src/components/ComponentFactory.js', function(exists) {
+fs.exists(LaravelMixPath + '/src/components/ComponentFactory.js', function(exists) {
 	console.log(exists);
     if (exists) {
     		let ComponentFactory = require(LaravelMixPath + '/src/components/ComponentFactory');
     		new ComponentFactory().installAll();
+    } else {
+        console.log(LaravelMixPath + '/src/components/ComponentFactory.js' + ' not exists');
     }
 });
 
